aee4d352c4c1380f85ab3e0d0ace71f108d130c8
Mohammed Mehdi <96487647+catosaurusrex2003@users.noreply.github.com>
2023-02-17 01:25:40 +0530

    stable all file transfer added

--- client/src/message.js
+++ client/src/message.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Message({data,left}) {
+function Message({ data, left }) {
   if (data.type == "file") {
-      const base64String = btoa(String.fromCharCode(...new Uint8Array(data.body)));
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(data.body)));
+    // IMAGES 
+    if (data.mimetype == "image/png" || data.mimetype == "image/jpeg") {
+      console.log("png")
       return (
-        <div className={`message ${left?"message-right":"message-left"}  `}>
+        <div className={`message ${left ? "message-right" : "message-left"}  `}>
           <div className='message-author'>{data.author}</div>
-          <img src={left?`${URL.createObjectURL(data.body)}`:`data:image/png;base64,${base64String}`} alt="" className='message-img' />
+          <img src={left ? `${URL.createObjectURL(data.body)}` : `data:image/png;base64,${base64String}`} alt="" className='message-img' />
           <div className='message-timestamp'>{data.time}</div>
         </div>
       )
-    // }
-    
+    }
+
+    // CAN BE ANY FILE , PDF , ZIP , EXCEL , DOC , MP3 , MP4 , TABLEAU
+    else {
+      return (
+        <div className={`message ${left ? "message-right" : "message-left"}  `}>
+          <div className='message-author'>{data.author}</div>
+          <a className='message-text download-link ' href={left ? `${URL.createObjectURL(data.body)}` : `${URL.createObjectURL(new Blob([data.body]))}`} download={data.fileName}>{data.fileName}</a>
+          <div className='message-timestamp'>{data.time}</div>
+        </div>
+      )
+    }
+
+
+
+
   }
-  else if(data.type = "message")
+  else if (data.type = "message")
     return (
-      <div className={`message ${left?"message-right":"message-left"}  `}>
+      <div className={`message ${left ? "message-right" : "message-left"}  `}>
         <div className='message-author'>{data.author}</div>
         <div className='message-text'>{data.message}</div>
         <div className='message-timestamp'>{data.time}</div>
