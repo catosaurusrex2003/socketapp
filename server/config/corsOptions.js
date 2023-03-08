8af558d576cde9c1b5f4e5b2119caa6e845425a4
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2023-03-09 01:33:46 +0530

    cors policy changed

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     cors: {
-        origin: ["http://localhost:3000","http://127.0.0.1:5173","https://mohammed-chat.netlify.app"]
+        origin: ["http://localhost:3000","http://127.0.0.1:5173","http://127.0.0.1:5174","https://mohammed-chat.netlify.app"]
     }
 }
 
