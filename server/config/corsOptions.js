fc0c4957f4c6d124cedf031e1595f5e62df57067
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2023-04-12 02:53:34 +0530

    dir name changed

--- server/config/corsOptions.js
+++
@@ -1,7 +0,0 @@
-const corsOptions = {
-    cors: {
-        origin: ["http://localhost:3000","http://127.0.0.1:5173","http://127.0.0.1:5174","https://mohammed-chat.netlify.app"]
-    }
-}
-
-module.exports = corsOptions
